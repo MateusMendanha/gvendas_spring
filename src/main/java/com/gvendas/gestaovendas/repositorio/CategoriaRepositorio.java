214333679951d50627aa918251b73d764bc42049
Mateus <mateusmendanha@gmail.com>
2021-06-06 11:43:53 -0300

    Add find products on database

--- src/main/java/com/gvendas/gestaovendas/repositorio/CategoriaRepositorio.java
+++ src/main/java/com/gvendas/gestaovendas/repositorio/CategoriaRepositorio.java
@@ -3,7 +3,6 @@ package com.gvendas.gestaovendas.repositorio;
 import com.gvendas.gestaovendas.entidades.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface CategoriaRepositorio extends JpaRepository<Categoria, Long> {
 
