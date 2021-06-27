fb73dfe953c2cd281c525f0a12bf8ba6fabf0fd6
Mateus <mateusmendanha@gmail.com>
2021-06-27 11:43:09 -0300

    Add api rest for query client on database

---
+++ src/main/java/com/gvendas/gestaovendas/controlador/ClienteControlador.java
@@ -0,0 +1,41 @@
+package com.gvendas.gestaovendas.controlador;
+
+
+import com.gvendas.gestaovendas.entidades.Cliente;
+import com.gvendas.gestaovendas.servico.ClienteServico;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Api(tags = "Cliente")
+@RestController
+@RequestMapping("/cliente")
+public class ClienteControlador {
+
+    @Autowired
+    private ClienteServico clienteServico;
+
+    @ApiOperation(value = "Listar", nickname = "listarTodos")
+    @GetMapping
+    public List<Cliente> listarTodos(){
+        return clienteServico.listarTodos();
+    }
+
+
+    @ApiOperation(value = "Listar", nickname = "buscarPorId")
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long codigo) {
+        Optional<Cliente> cliente = clienteServico.buscarPorCodigo(codigo);
+        return cliente.isPresent() ? ResponseEntity.ok(cliente.get()) : ResponseEntity.notFound().build();
+    }
+
+}
